84f805dd074bafcb0e46614c01ed7f25dc18792f
Sheldrick <mrmayrant@yahoo.com>
2019-08-11 07:47:35 -0400

    retrieved the posts from the API and displayed it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-	replaceMe: () => "fake reducer to be replaced"
+	posts: postsReducer
 });
